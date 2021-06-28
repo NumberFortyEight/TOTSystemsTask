f4a919a88b9684b66980fbd4b809508f1191acbe
NumberFortyEight <pavellakhtin@mail.com>
2021-06-28 17:43:59 +0500

    Initial commit

---
+++ src/main/java/com/example/TOTTask/TotTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.TOTTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TOTTask/TotTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TOTTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
